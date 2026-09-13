8b1c170e4b43bd482ffdc7393a9d6f5664f020a0
alnuqtamedia <alnuqtamedia@gmail.com>
2026-09-13 03:43:28 +0300

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -2,29 +2,38 @@ import { defineConfig } from "tinacms";
 
 export default defineConfig({
   branch: "main",
-  clientId: "cc09f18c-8a8e-4305-a776-2534e9ebb90d", // الرمز الخاص بحسابك
-  token: "", // اتركه فارغاً حالياً أو أضف الـ Read-only Token من تينا
+  clientId: "cc09f18c-8a8e-4305-a776-2534e9ebb90d",
+  token: "",
   build: {
     outputFolder: "admin",
     publicFolder: "public",
   },
   media: {
     tina: {
-      mediaRoot: "uploads",
+      mediaRoot: "",
       publicFolder: "public",
     },
   },
   schema: {
     collections: [
       {
-        name: "investigations",
-        label: "التحقيقات الصحفية",
-        path: "content/investigations",
+        name: "post",
+        label: "المقالات والأخبار",
+        path: "content/posts",
         fields: [
-          { type: "string", name: "title", label: "عنوان التحقيق", isTitle: true, required: true },
-          { type: "datetime", name: "date", label: "تاريخ النشر" },
-          { type: "string", name: "summary", label: "الملخص" },
-          { type: "rich-text", name: "body", label: "نص التحقيق", isBody: true },
+          {
+            type: "string",
+            name: "title",
+            label: "العنوان",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "محتوى المقال",
+            isBody: true,
+          },
         ],
       },
     ],
